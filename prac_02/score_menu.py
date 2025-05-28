d8026e0b75bc9fa0da1826bbbaa06baf27aa4b43
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-05-28 22:09:44 +1000

    Resolve mistake with input function

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -22,7 +22,7 @@ MENU = "(G)et a valid score (0-100)\n(P)rint result\n(S)how stars\n(Q)uit"
 def main():
     """Function docstring"""
     print(MENU)
-    user_selection = input(str("Choose an option from above :"))
+    user_selection = str(input("Choose an option from above :"))
     
     while user_selection != "Q":
         if user_selection == "G":
@@ -34,7 +34,7 @@ def main():
             print(evaluate_score(score))
         
         print(MENU)
-        user_selection = input(str("Choose an option from above :"))
+        user_selection = str(input("Choose an option from above :"))
 
 
 def get_score():
