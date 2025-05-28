b2d02116427bd76d4dc5b82b822f95df397a1a88
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-05-28 21:46:42 +1000

    Create file and layed out templates

---
+++ prac_02/score_menu.py
@@ -0,0 +1,35 @@
+'''display menu
+get choice
+while choice != <quit option>
+    if choice == <first option>
+        <do first task>
+    else if choice == <second option>
+        <do second task>
+    ...
+    else if choice == <n-th option>
+        <do n-th task>
+    else
+        display invalid input error message
+    display menu
+    get choice
+<do final thing, if needed>'''
+
+
+"""James Dixon-Mills Practical 2"""
+
+from score import evaluate_score
+# imports
+# CONSTANTS
+
+def main():
+    """Function docstring"""
+    # statements...
+    do_stuff()
+
+
+def do_stuff():
+    """Function docstring"""
+    # statements...  
+
+
+main()
