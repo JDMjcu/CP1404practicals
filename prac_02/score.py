841bb6599f5c6e69b1caead39a1523aabfe1d1af
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-05-28 21:40:56 +1000

    Add random aspect to score

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,18 +3,19 @@ James Dixon-Mills
 CP1404/CP5632 - Practical 2
 program to determine score status
 """
- 
-    
-"""Module docstring"""
+import random
 
 def main():
-    """Function docstring"""
+    """Recieves score input and prints the evaluation"""
     score = float(input("Enter score: "))
     print(evaluate_score(score))
 
+    random_score = random.randint(0,100)
+    print(evaluate_score(random_score))
+
 
 def evaluate_score(score):
-    """Function docstring"""
+    """Seperates the score into 4 evaluations"""
     if score < 0 or score > 100:
         return "Invalid Score"
     elif score >= 90:
