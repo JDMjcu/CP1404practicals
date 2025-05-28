5d960e292139b91b6d461726e8a0408c7cf4fa4e
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-05-28 21:36:04 +1000

    Refactor program to use functions

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,26 @@ James Dixon-Mills
 CP1404/CP5632 - Practical 2
 program to determine score status
 """
-score = float(input("Enter score: "))
+ 
+    
+"""Module docstring"""
 
-if score < 0 or score > 100:
-    print("Invalid Score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    """Function docstring"""
+    score = float(input("Enter score: "))
+    print(evaluate_score(score))
+
+
+def evaluate_score(score):
+    """Function docstring"""
+    if score < 0 or score > 100:
+        return "Invalid Score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"  
+
+
+main()
