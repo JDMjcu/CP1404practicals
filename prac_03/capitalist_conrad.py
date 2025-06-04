522900aedcc99b34274721ef92c3813d68729f3e
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-04 12:31:51 +1000

    Implement string formatting and ammend constants

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,12 +9,13 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100
 INITIAL_PRICE = 10.0
 
+number_of_days = 1
 price = INITIAL_PRICE
 print(f"${price:,.2f}")
 
@@ -32,4 +33,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is: {price:,.2f}")
+    number_of_days += 1
