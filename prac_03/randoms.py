c08797abea0d03ec9aac26afdc82a02286e2b792
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-04 12:16:43 +1000

    Complete questions and generate random number from 1-100 inclusive

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -13,16 +13,17 @@ print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
 
-'''
-    What did you see on line 1?
-    What was the smallest number you could have seen, what was the largest?
 
-    What did you see on line 2?
-    What was the smallest number you could have seen, what was the largest?
-    Could line 2 have produced a 4?
+# What did you see on line 1? A set of numbers ranging from 5-20 
+# What was the smallest number you could have seen, what was the largest? 5 and 20
 
-    What did you see on line 3?
-    What was the smallest number you could have seen, what was the largest?
+# What did you see on line 2? 3,5,3,7,3,9
+# What was the smallest number you could have seen, what was the largest? 3 and 9 should be odd numbers
+# Could line 2 have produced a 4? No, cause 4 is not an odd number 
 
-    Write code, not a comment, to produce a random number between 1 and 100 inclusive.
-'''
+# What did you see on line 3? 4.077355911500646,
+# What was the smallest number you could have seen, what was the largest? smallest 2.5375533070385754, largest  4.1515390891622586, 
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+print(random.randint(1, 100))
