b1178c131b85a34fff3faed18e655b7887323e8e
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-07 14:57:01 +1000

    Answer questions 1 and 2

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -1,8 +1,8 @@
 """
 CP1404/CP5632 - Practical
 Answer the following questions:
-1. When will a ValueError occur?
-2. When will a ZeroDivisionError occur?
+1. When will a ValueError occur? When an integer is not entered on either line 10/11
+2. When will a ZeroDivisionError occur? When 0 is inputed into the demoninator variable
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
 """
 
