f0af6c1081d3134adf68f326c9c168ec8c379445
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-07 15:48:13 +1000

    Ammend todo 1 and start todo 2

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -28,7 +28,7 @@ def main():
 def is_valid_password(password):
     """Determine if the provided password is valid."""
        
-    if password < MIN_LENGTH or password > MAX_LENGTH:
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
     
     number_of_lower = 0
@@ -40,10 +40,15 @@ def is_valid_password(password):
         # TODO: count each kind of character (use str methods like isdigit)
         # Referencing https://www.w3schools.com/python/python_ref_string.asp
         # test number of lower with islower, then amend the count
+        if character.islower:
+            number_of_lower += 1
+            
         # test number of upper with isupper, then amend the count
         # test number of digit with isdigit, then amend the count
         # test number of special digit by referencing to see if that is in SPECIAL_CHARACTERS 
 
+        print(number_of_lower,number_of_digit,number_of_special,number_of_upper)
+        
     # TODO: if any of the 'normal' counts are zero, return False
 
     # if upper, lower or digit = 0 
