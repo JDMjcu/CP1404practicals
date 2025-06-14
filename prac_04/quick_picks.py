fe74adaff33275f6d0c634c442647300c805ef58
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-14 19:22:27 +1000

    Introduce number checking to prevent duplicate numbers in a quick pick

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -24,6 +24,9 @@ for game in range(number_of_games):
     quick_pick = []
     for number in range(NUMBERS_PER_LINE):
         number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) 
+        while number in quick_pick:
+            # Check for number in quickpick if duplicate make a new number
+            number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) 
         quick_pick.append(number)
         quick_pick.sort()
     print(quick_pick)
