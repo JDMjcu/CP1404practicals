33f9e5685c31bd5b398ad36a376fe7d411f9ef41
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-14 18:26:16 +1000

    Complete lists_warmup

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,3 +11,25 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # "3" in numbers False there is no "3" in the list
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+# In the same Python file, write statements to achieve the following:
+
+#     Change the first element of numbers to "ten" (the string, not the number 10)
+#     Change the last element of numbers to 1
+#     Print all the elements from numbers except the first two (slice)
+#     Print whether 9 is an element of numbers
+
+
+# 1.
+numbers[0] = "ten"
+print(numbers)
+
+# 2.
+numbers[-1] = 1
+print(numbers)
+
+# 3. 
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
+
