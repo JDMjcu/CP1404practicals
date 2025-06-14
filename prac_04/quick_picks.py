f300f46bb5195f4da0591b4ff491bbc10703e000
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-14 19:18:11 +1000

    Start work on quick_picks

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,29 @@
+"""
+   James Dixon-Mills
+   Prac-04
+   
+   
+    Write a program that asks the user how many "quick picks" they wish to generate. The program then generates that many lines of output. Each line consists of 6 random numbers between 1 and 45.
+    These values should be stored as CONSTANTS.
+
+    Each line (quick pick) should not contain any repeated number.
+    Each line of numbers should be displayed in sorted (ascending) order.
+    Study the formatting below so that numbers align neatly.
+  
+"""
+
+import random
+
+NUMBERS_PER_LINE = 6
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+
+number_of_games = int(input("How many quickpicks do you want to play? "))
+
+for game in range(number_of_games):
+    quick_pick = []
+    for number in range(NUMBERS_PER_LINE):
+        number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER) 
+        quick_pick.append(number)
+        quick_pick.sort()
+    print(quick_pick)
