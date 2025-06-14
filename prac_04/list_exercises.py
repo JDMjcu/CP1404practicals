526b1e31f979b75e4a81142fd058f2675b7124d3
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-14 19:01:14 +1000

    Start Woefully inadequate security checker

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -17,3 +17,11 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average number is {sum(numbers)/len(numbers)}")
+
+
+# b
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup',
+            'NicolEye', 'swei45', 'BaseInterpreterInterface', 
+            'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 
+            'InterpreterInterface', 'StartServer', 'bob'] 
+
