38aff519768c623f6a7b319c7f1d1ca8ec97aeb9
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-14 18:59:19 +1000

    Complete the basic list operations section of list_exercises

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -11,3 +11,9 @@ for i in range(5):
     numbers.append(number)
     
 print("You entered:", numbers)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers)/len(numbers)}")
