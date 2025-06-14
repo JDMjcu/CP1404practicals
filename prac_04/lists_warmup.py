0c5da1a91c232f6373f51bf6be18fc33bcf1be1a
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-14 18:21:13 +1000

    Include lists_warmup practice questions

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,1 +1,13 @@
 numbers = [3, 1, 4, 1, 5, 9, 2]
+
+
+# numbers[0] = 3 
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] = 3, 1, 4, 1, 5, 9,
+# numbers[3:4] = 1 
+# 5 in numbers True
+# 7 in numbers False
+# "3" in numbers False there is no "3" in the list
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
