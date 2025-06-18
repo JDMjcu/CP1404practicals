499083eafd02f2ac1ed428c35e18efffc48e71ff
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-18 14:48:21 +1000

    Upload wimbledon and plan the task

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,20 @@
+"""
+Expected time: 30 Minutes 
+Actual time:  
+JDM CP1404 
+Prac 05  - Wimbledon
+"""
+
+
+# Planning
+# create 4 fucntions
+# main
+# read_data
+    # Data is recorded as Year,Country,Champion,Country,Runner-up,Score in the final 
+    
+# process_data
+    # score in the final shows how many games they have won by the amount of scores
+# display results
+    # the display should print the champions and the amount of times they have won
+    # the countries of the champions in alphabeticlal order
+    
