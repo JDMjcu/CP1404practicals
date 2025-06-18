cd7ec8e92fdd9ab3830528dd78edc3366a081bc6
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-18 12:22:18 +1000

    Remove working loop

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -6,13 +6,9 @@ JDM - 2025
 """
 
 
-COLOUR_TO_CODE = {"absolute Zero": "#0048ba", "amethyst": "#9966cc", "baby pink": "#f4c2c2", "banana yellow": "#ffe135",
+COLOUR_TO_CODE = {"absolute zero": "#0048ba", "amethyst": "#9966cc", "baby pink": "#f4c2c2", "banana yellow": "#ffe135",
                   "generic viridian": "#007f66", "royalblue1": "#4876ff", "ghostwhite": "#f8f8ff", "india green": "#138808",
                   "tomato1": "#ff6347", "sienna2": "#ee7942"}
-
-print("The colours in the dictionary are:")
-for colour in COLOUR_TO_CODE:
-    print(colour)
     
 colour = input("Enter colour: ").lower()
 while colour != "":
