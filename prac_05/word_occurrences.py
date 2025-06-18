efbc6a4165faafb5f3e6f633cc1d8526aca467cb
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-18 13:26:27 +1000

    Amend code to rename variables and sort words for printing

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,37 +1,26 @@
 """ 
 Expected time: 20  Minutes 
-Actual time: 18 Minutes
+Actual time: 18 Minutes for first ver, 30 minutes after fixing naming and sorting the list
 JDM CP1404 
 Word counter
 """
 
 
-sentence = input("Text: ")
+text = input("Text: ")
 
-words = sentence.split(" ")
-
-print(words)
+# Use .lower for the case of "A bee can be a queen and I can be a human" 
+# prevents A from being classed as its own word.
+words = text.lower().split(" ") 
 
 word_to_count = {}
 
-
-# Count the number of times word is in list of words
-# add that to the dictionary
 for word in words:
     frequency = words.count(word)
     word_to_count[word] = frequency
-    
-print (word_to_count)
 
-# to print nicely 
-# get max length of charater use list comprehension and max function
-# occording to criteria need to sort so most frequent are higher in the print
-
-unique_words = list(word_to_count.keys())
+unique_words = sorted(list(word_to_count.keys()))
 
 maximum_length = max(len(word) for word in unique_words)
 
 for word in unique_words:
     print(f"{word:{maximum_length}} : {word_to_count[word]}")
-
-
