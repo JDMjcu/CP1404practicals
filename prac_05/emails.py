25e44e8ab4d6e3df44cbfac5577f2a4b0145c8b8
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-18 14:19:47 +1000

    Amend code to remove abiguious 15 and remove working comments

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -6,25 +6,27 @@ Prac 05  - Email task
 """
 def main():
     """ Create dictionary of email to name."""
-    email_to_name = {} # plan to do something like email : "Name"
+    email_to_name = {} 
     email = input("Email: ")
+    
     while email != "":
         name = get_name_from_email(email)
-        # make a verification check for either Y or "" as yes and anything else as no
+        
         verification = input(f"Is your name {name}? (Y/n)")
+        
         if verification.upper() != "Y" and verification != "":
             name = input("Name: ")
+        
         email_to_name[email] = name
         email = input("Email: ")
         
-        
-    # format the names and email adresses
+    max_length = max(len(name) for name in email_to_name.values())    
+    
     for email, name in email_to_name.items():
-        print(f"{name:15} : {email}")   
+        print(f"{name:{max_length}} : {email}")   
         
 def get_name_from_email(email):
     """ Exctract a name from the email assuming its firstname.lastname@email """
-    # Treat an email as identifier@email
     identifier = email.split("@")[0]
     names = identifier.split(".")
     fullname = " ".join(names).title()
