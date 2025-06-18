7db4c5c7f4fee7fa96fbd796374ef3c1fec36c59
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-06-18 16:30:11 +1000

    Create loop to record the amount of times a winner won the wimbledon

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,5 +1,5 @@
 """
-Expected time: 30 Minutes 
+Expected time: 30 Minutes initially 60 minutes after starting it
 Actual time:  7 minutes for read function,
 JDM CP1404 
 Prac 05  - Wimbledon
@@ -17,20 +17,39 @@ Prac 05  - Wimbledon
 # display results
     # the display should print the champions and the amount of times they have won
     # the countries of the champions in alphabeticlal order
-
-
-
-
 FILENAME = "wimbledon.csv"
 
-def read_data(FILENAME):
+
+
+def main():
+    """Read the file and output details for the wimbledon"""
+    records = read_record(FILENAME)
+    winner_to_number_of_wins = process_data(records)    
+
+
+def read_record(FILENAME):
     """Reads wimbledon.csv and stores it as a list"""
     records = []
     with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
-        for line in in_file:
+        for line in in_file.readlines()[1:]: # resolve error with champion being included in the process data
             parts = line.strip().split(",")
             records.append(parts)  
-        
-        print(records)
+            
+        return(records)
 
-read_data(FILENAME)
+
+def process_data(records):
+    winner_to_victories = {} # Winner, : count
+    for record in records:
+        winner = record[2]
+        if winner in winner_to_victories:
+            winner_to_victories[winner] += 1
+        else:
+            winner_to_victories[winner] = 1
+    print(winner_to_victories)
+    
+    
+    
+        
+
+main()
