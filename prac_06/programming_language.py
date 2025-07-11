a8c595081967ca40fd09fabb6d163abe46f884d1
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-07-11 20:41:47 +1000

    Amend docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -4,7 +4,7 @@
 """
 
 class ProgrammingLanguage():
-    """Bla bla b;a"""
+    """Represent programing languages as objects"""
     
     def __init__(self, name = '', typing = '', reflection = '', year = '0'):
         """Construct a Programming language based of the provided details"""
@@ -14,14 +14,11 @@ class ProgrammingLanguage():
         self.typing = typing
         
     def __str__(self):
+        "Return a string for the language."
         return (f"{self.name}, {self.typing} Typing , Reflection = {self.reflection}, First appeared in {self.year}")
 
-        
-        
-        
-        
-        
     def is_dynamic(self):
+        "Determine if the language is Dynamic."
         return self.typing == "Dynamic"
     
     
