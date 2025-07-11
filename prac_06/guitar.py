e0521c4975fa9f347e3b586050aa82d0ae3c845b
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-07-11 20:39:18 +1000

    Amend guitar for readability

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -13,19 +13,22 @@ class Guitar():
     """This creates a guitar object"""
     
     def __init__(self, name = '', year = 0, cost = 0.0):
-        """creates a guitar object with provided details"""
+        """Records details of the guitar."""
         self.name = name
         self.year = year
         self.cost = cost
     
     def __str__(self):
+        """Return a string of the guitar."""
         return f"{self.name} ({self.year}) : ${self.cost}"
     
     
     def get_age(self):
+        """Gets the age of the guitar."""
         return 2025 - self.year
     
     def is_vintage(self):
+        """Determine if a Guitar is considered vintage."""
         return True if self.get_age() >= 50 else False
     
     
