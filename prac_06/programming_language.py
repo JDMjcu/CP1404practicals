20537d80bfd36965c489f786544ebf737d362f9d
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-07-11 19:15:48 +1000

    Create ProgrammingLanguage class and init function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+""" CP1404- prac 6
+    Programming language task
+    James Dixon-Mills
+"""
+
+class ProgrammingLanguage():
+    """Bla bla b;a"""
+    
+    def __init__(self, language, typing, reflection, year):
+        """Construct a Programming language based of the provided details"""
+        self.language = language
+        self.reflection = reflection
+        self.year = year
+        self.typing = typing
+        
