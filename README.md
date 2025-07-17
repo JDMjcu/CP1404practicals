3f04ad359ff38c85eb070a17b115ec92bcb7aaec
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-07-17 18:28:52 +1000

    Amend programming_language to check for pointer arithmetic

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## About This Repository
 
-My name is James, and I’m a second-year Civil Engineering student at James Cook University. I chose CP1404 – Programming II as part of my minor—although I can't exactly remember why, other than thinking it might be fun.
+My name is James, and I’m a second-year Civil Engineering student at James Cook University. I chose CP1404 – Programming II as part of my minor because i thought it would be fun.
 
 This repository contains my practical work for the subject **CP1404 - Programming II** at James Cook University. 
 It includes weekly coding exercises and assignments written in Python, 
@@ -15,7 +15,7 @@ I’ve learned that clean code does the following:
 - Follows the Single Responsibility Principle (SRP): This makes your code modular, easier to debug, and in my opinion easier to understand.
 - Does not have magic numbers. Writing MAX_LENGTH = 12 is much clearer than a hard-coded :12 in your strings or loops.
 
-## Helpful Resources (Things that kept me going)
+## Helpful Resources 
 
 - [Programming Patterns](https://github.com/CP1404/Starter/wiki/Programming-Patterns) – An essential reference for structuring your tasks
 - [CP1404 Practicals Instructions](https://github.com/CP1404/Practicals/tree/master) – Weekly practical tasks and instructions that guided this work.
--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,12 +7,13 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.arithmetic = arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
@@ -22,14 +23,19 @@ class ProgrammingLanguage:
         """Determine if language is dynamically typed."""
         return self.typing == "Dynamic"
 
+    def is_pointer_arithmetic(self):
+        """Determine if language uses Pointer Arithmetic"""
+        return self.arithmetic == "Pointer"
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, "None")
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991, "None")
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991, "None")
+    cplusplus = ProgrammingLanguage("C++", "Static", False, 1983, "Pointer")
 
-    languages = [ruby, python, visual_basic]
+
+    languages = [ruby, python, visual_basic,cplusplus]
     print(python)
 
     print("The dynamically typed languages are:")
@@ -37,6 +43,11 @@ def run_tests():
         if language.is_dynamic():
             print(language.name)
 
+    
+    print("\nLanguages that support pointer arithmetic:")
+    for language in languages:
+        if language.is_pointer_arithmetic():
+            print(language.name)
 
 if __name__ == "__main__":
     run_tests()
