ae9271485c67e14ba33ecba55f4d6365ee84c581
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-07-20 12:41:30 +1000

    Plan out project_management task

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,7 +6,7 @@ class Project():
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage ):
         """ Initialise a Project instance. """
         self.name = name
-        # self.start_date = start_date Do after everything else
+        self.start_date = datetime.datetime.strptime(start_date, "%d/%m/%Y").date()
         self.priority = int(priority)
         self.cost_estimate = float(cost_estimate)
         self.completion_percentage = float(completion_percentage)
@@ -18,4 +18,15 @@ class Project():
     def __lt__(self, other):
         return self.priority < other.priority
     
+    def __str__(self):
+        #Example str Organise Pantry, start: 20/07/2022, priority 1, estimate: $25.00, completion: 55%
+        return (f"{self.name}, Start: {self.start_date.strftime('%d/%m/%Y')}, priority {self.priority}, estimate: ${self.cost_estimate}, completion: {self.completion_percentage}%")
     
+    
+# Test code
+    
+# project1 = Project("Porject", "12/12/2025", 4, 0, 0.2)
+# project2 = Project("This Class", "1/2/2025", 2, 0, 30)
+# print(project1)
+# print(project1.is_complete())
+# print(project1 > project2)
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -2,8 +2,82 @@
 CP1404 prac07 
 Project managenent task
 Estimated time: 
-                10 Minutes for Class
-                1:40 for the programm
+                10 Minutes for Class nope => 20 minutes (actual) 
+                2 hr for the programm
                 Start time: 11:50
+                took a break froom 12:40 ->
 
 """
+
+from project import Project
+import datetime
+DEFAULT_FILENAME = "projects.txt"
+
+def main():
+    """ Run the pythonic project managment code program. """
+    
+    print("Welcome to Pythonic Project Management")
+    
+    # Load files from default filename
+    projects = load_projects(DEFAULT_FILENAME)
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILENAME}")
+    
+    # Create menu
+    menu = "- (L)oad projects\n- (S)ave projects\n- (D)isplay projects\n- (F)ilter projects by date\n- (A)dd new project\n- (U)pdate project\n- (Q)uit"
+    print(menu)
+    choice = input(">>> ")
+    
+    while choice.lower() != "q":
+        if choice == "l":
+            filename = input("Filename: ")
+            projects = load_projects(filename)
+        elif choice == "s":
+            filename = input("Filename: ")
+            save_projects(filename, projects)
+        elif choice == "d":
+            display_projects(projects)
+        elif choice == "f":
+            filter_projects_by_date(projects)
+        elif choice == "a":
+            add_new_project(projects)
+        elif choice == "u":
+            update_project(projects)
+        else:
+            print("Invalid choice.")
+        print(menu)
+        choice = input(">>> ").lower()
+    
+    
+    save_query = input("Would you like to save to {DEFAULT_FILENAME}?")
+    if save_query.lower() == "yes" or "y":
+        save_projects(DEFAULT_FILENAME, projects)
+    print("Thank you for using custom-built project management software.")
+
+
+def load_projects(filename):
+    """Create a list of projects from a file. """
+    print(f"dosomething with {filename}")
+    return [1,2,3,4,5]
+
+def save_projects(filename, projects):
+    """ Save a list of projects to a file. """
+    print(f"dosomething with {filename}")
+    
+def display_projects(projects):
+    """ Display the list of projects when prompted. """
+    print(f"dosomething with {projects}")
+
+def filter_projects_by_date(projects):
+    """ Filter the list of projects by date instead of priority."""
+    print(f"dosomething with {projects}")
+    
+def add_new_project(projects):
+    """ Add a project to the list of projects with user input. """
+    print(f"dosomething with {projects}")
+    
+def update_project(projects):
+    """ Update a project in the list of projects."""
+    print(f"dosomething with {projects}")
+    
+
+main()
