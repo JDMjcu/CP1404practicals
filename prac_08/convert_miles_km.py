867c179599f144c492c4c40a65bd18ab3c95c632
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-07-27 16:58:48 +1000

    Amend handle_calculations error handling

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -8,7 +8,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import StringProperty
 
-MILESINKILOMETER = 1.609344 # 1 Mile = 1.609344 Kilometers
+MILES_IN_KILOMETER  = 1.609344 # 1 Mile = 1.609344 Kilometers
 
 class ConvertMilesApp(App):
     """ ConvertMilesApp is a Kivy App for converting miles to kilometers """
@@ -23,11 +23,11 @@ class ConvertMilesApp(App):
     def handle_calculate(self, value):
         """ Converts Miles to kilometes when button is pressed, output result to label widget. """
         try:
-            result = float(value) * MILESINKILOMETER
-            self.output_text = str(result) 
-        except ValueError:
-            pass
-    
+            result = float(value) * MILES_IN_KILOMETER
+            self.output_text = str(result)
+        except (ValueError, TypeError):
+            self.output_text = "0.0"  
+            
     def handle_increment(self, value, direction):
         try:
             number = float(value) + direction
