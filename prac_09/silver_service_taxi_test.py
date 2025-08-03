9a40f9d6b02f65e22b57ee03f73e2f9714e16836
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-08-03 19:37:16 +1000

    Complete Inheriting Enhancements task

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -17,4 +17,5 @@ test_taxi.start_fare()
 test_taxi.drive(18)
 taxi_fare = taxi.get_fare()
 print(f"Fare for 18km trip: ${taxi_fare:.2f}")
-assert taxi_fare == 48.78, f"Expected fare: $48.78, got: ${taxi_fare:.2f}"
+# assert taxi_fare == 48.78, f"Expected fare: $48.78, got: ${taxi_fare:.2f}"
+assert taxi_fare == 48.80, f"Expected fare: $48.78, got: ${taxi_fare:.2f}"
