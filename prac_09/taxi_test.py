654cbc27d346a58659595aa15a6eada23062981b
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-08-03 19:03:20 +1000

    Complete Class Variables task

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -2,8 +2,8 @@ from taxi import Taxi
 
 def main():
     """Test the taxi calss provided."""
-    price_per_km = 1.23
-    my_taxi = Taxi("Prius 1", 100, price_per_km)
+    
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
