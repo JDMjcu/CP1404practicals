265fbe4f282a29f45ce8616a2236ad86848b83a3
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-08-03 19:01:41 +1000

    Complete first part of taxi_test

---
+++ prac_09/taxi_test.py
@@ -0,0 +1,14 @@
+from taxi import Taxi
+
+def main():
+    """Test the taxi calss provided."""
+    price_per_km = 1.23
+    my_taxi = Taxi("Prius 1", 100, price_per_km)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(my_taxi)
+
+
+main()
