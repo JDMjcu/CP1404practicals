a12df5c8581b5957ef750416cec4972cdd7c74a7
JDMjcu <james.dixonmills@my.jcu.edu.au>
2025-08-03 19:19:02 +1000

    Amend the unreliable_car_test

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -4,7 +4,6 @@ from unreliable_car import UnreliableCar
 my_unreliable_car = UnreliableCar("TheUnreliableMobile", 100, 40)
 
 for i in range(20):
-    print(f"Attempting to drive {i}km:")
-    print(f"{my_unreliable_car.name} drove {my_unreliable_car.drive(1)}")
+    print(f"{my_unreliable_car.name} is attempting to drive {i}km: it drove {my_unreliable_car.drive(1)}")
 
 print(my_unreliable_car)
